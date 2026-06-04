d64bd66719dc667decfde891df43f6eabdf14ba9
kevinsanchez4010 <kevin.sanchez4010@utc.edu.ec>
2026-06-04 15:23:49 -0500

    vistas

---
+++ inicio/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('cart/', views.cart, name='cart'),
+    path('contact/', views.contact, name='contact'),
+    path('404/', views.error_404, name='error_404'),
+]
