914eea0b01ec125bfaec9e7ffecc1796045766fd
kevinsanchez4010 <kevin.sanchez4010@utc.edu.ec>
2026-06-04 15:37:43 -0500

    view shop

--- inicio/urls.py
+++ inicio/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('contact/', views.contact, name='contact'),
     path('404/', views.error_404, name='error_404'),
+    path('shop/', views.shop, name='shop'),
 ]
