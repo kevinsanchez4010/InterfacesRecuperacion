232833a6e05a8c32896622c5c3a0a5298db94fb0
leninchango8768 <lenin.chango8768@utc.edu.ec>
2026-06-04 14:43:37 -0500

    Lenin añadir static en settings

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[
+    BASE_DIR / 'static',
+]
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
